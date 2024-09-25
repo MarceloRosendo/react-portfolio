8af9e895258ff779c1576afc73a4052f2ef11c39
MarceloRosendo <marcelomax159@gmail.com>
2024-09-24 21:16:00 -0300

    feat: upgrading information

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
-import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+import javaIcon from "@iconify/icons-logos/java";
+import reactIcon from "@iconify/icons-logos/python";
+import vueIcon from "@iconify/icons-logos/nodejs";
 
 class About extends Component {
   render() {
@@ -31,7 +31,7 @@ class About extends Component {
                     alt="Avatar placeholder"
                   />
                   <Icon
-                    icon={angularIcon}
+                    icon={javaIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
